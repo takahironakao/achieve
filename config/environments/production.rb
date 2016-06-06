5f803d414a4ae33864d4a36c690aac813b2c80cb
Takahiro Nakao <saboten571111@yahoo.co.jp>
2016-06-06 06:51:30 +0000

    “first_commit”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'fathomless-tor-29250.herokuapp.com/' }
   
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
