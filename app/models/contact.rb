3b4b5ee5102f0ec49f3549c896ebd0317a30e3b1
Takahiro Nakao <saboten571111@yahoo.co.jp>
2016-06-08 07:53:24 +0000

    tree commit

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,6 @@ class Contact < ActiveRecord::Base
     validates :email, presence: true
     validates :content, presence: true
     default_scope -> { order(created_at: :desc) }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
