f5c724e5908092f08f0cbc7e3acf845d8db162c0
Takahiro Nakao <saboten571111@yahoo.co.jp>
2016-06-08 07:40:48 +0000

    tree commit

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -8,7 +8,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_blog(blog)
     @blog = blog
 
-    mail to: @blog.email ,　subject: '【Achieve】ブログが投稿されました'
+    mail to: @User.email ,　subject: '【Achieve】ブログが投稿されました'
   end
   
   def sendmail_contact(contact)
